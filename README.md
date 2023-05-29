218ea307b6427d0553e7639de9a103b34f5fcae6
Rain305 <30487503+BiaxialRain305@users.noreply.github.com>
2023-05-29 10:40:37 -0400

    added new files

---
+++ README.md
@@ -0,0 +1,15 @@
+<h1>Automatically create a wishlist from your clipboard</h1>
+
+<h3>Instructions:</h3> 
+	<li><a href="https://github.com/BiaxialRain305/CreateWl/archive/refs/heads/main.zip">>>Latest Download></a></li> 
+	<li><a href="https://www.python.org/downloads/">Download the latest version of Python.</a></li>
+	<li>From there connect a macro to run the makeWl.py file through a key on your keyboard.</li>
+<h4>Side Notes:</h4>
+<ul>
+	&emsp;-Some applications, like ICUE, don't allow the use of .py files to run through the application.<br/>
+	&emsp;-If this is the case, simply use the .bat file. The .bat file is a script that simply runs makeWl.py. Therefore, both files are needed still!<br/>
+	&emsp;-To remove the command prompt popup that appears for a second, follow these steps below:<br/>
+	&emsp;1. Hit the Windowns key, type "Choose a default app for each type of file".<br/>
+	&emsp; <img src ="https://cdn.discordapp.com/attachments/683159590229245960/945459231623086240/PpSUPhO.png" width="620" height="450"><br/>
+	&emsp;2. Scroll down until you see ".py" and make sure the file type ".py" has a default app of Python.<br/>
+</ul>
---
+++ makeWl.py
@@ -0,0 +1,74 @@
+import pyperclip
+
+
+def main():
+    inp = pyperclip.paste().splitlines()
+    temp2 = ''
+    for i in inp:
+        if not i or 'Page ' and ' / ' in i or 'Total value: ' in i or ':kakera:\r' in i or '\u200b' in i \
+                or 'Top 15 value: ' in i or 'AVG: ' in i or '🏆 TOP ' in i and ' TOP 1000' in i \
+                or ' $wa, ' in i and ' $ha, ' in i and ' $wg, ' in i and ' $hg' in i and '(' not in i and ')' not in i \
+                or ' wishlist (' in i and ')' in i:
+            inp = inp[+1:]
+            continue
+
+        elif i[0] == '$':
+            inp = pyperclip.paste()
+            return inp
+
+        temp = i
+
+        if temp[0] == '#':
+            temp = i.split(' - ')[1]
+
+        temp = \
+            temp.split(' · ')[0].replace('💞', '').replace('✅', '').replace('⭐', '').replace('🔐', '').replace(
+                '❌', '').replace('🚫', '').replace(':kakera:', '').strip().split('  ')[0].split(' 🚫 ')[0].split(
+                '(Soulkeys: ')[0].split(
+                ' - Touhou')[0].split(' | ')[0].split(' => ')[0].split(' ⚠')[0]
+
+        if temp[len(temp) - 3:] == ' ka' or ' (' in i and ')' in i or ' - Touhou' in i:
+            if temp.count(' - ') > 1:
+                temp = temp.split(' - ')[0] + ' - ' + temp.split(' - ')[1]
+            else:
+                temp = temp.split(' - ')[0]
+            print(temp)
+
+        if temp[len(temp) - 3:] == ' ka':
+            remove_num = temp[:len(temp) - 3]
+            while remove_num[len(remove_num) - 1].isnumeric():
+                remove_num = remove_num[:-1]
+
+            temp = remove_num
+
+        # DL Making Below and t flag without rank
+        remove_dl = temp
+        if remove_dl[len(remove_dl) - 1] == ')':
+            remove_dl = remove_dl[:-1]
+            while remove_dl[len(remove_dl) - 1].isnumeric() or remove_dl[len(remove_dl) - 1] == '(':
+                remove_dl = remove_dl[:-1]
+
+                if not remove_dl[len(remove_dl) - 1].isnumeric() or remove_dl[len(remove_dl) - 1] != '(':
+                    break
+
+            if remove_dl[len(remove_dl) - 3:] == '$hg':
+                remove_dl = remove_dl.split(' ~ ')[0]
+
+            while remove_dl[len(remove_dl) - 1].isnumeric() or remove_dl[len(remove_dl) - 1] == '(' and remove_dl[
+            len(remove_dl) - 2] == ' ':
+                remove_dl = remove_dl[:-1]
+                temp = remove_dl
+
+        # DL Making Above ^
+
+        if inp[0] == i:
+            temp2 = temp2 + temp  # + "\n"
+        else:
+            temp2 = temp2 + "$" + temp  # + "\n"
+    # temp2.replace('\n', '')
+    inp = temp2
+    return pyperclip.copy(inp)
+
+
+if __name__ == '__main__':
+    main()
